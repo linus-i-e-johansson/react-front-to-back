9d95d0a4519f3982eba3fa720e15d257fc6c30da
Linus <linus.react.projects@gmail.com>
2020-08-01 14:40:47 +0200

    worked on context, work in progress.

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -42,7 +42,8 @@ const GithubState = props => {
             users:state.users,
             user: state.user,
             repos:state.repos,
-            loading: state.loading
+            loading: state.loading,
+            searchUsers
     }}
     >
         {props.children}
--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -1,5 +1,27 @@
-import {createContext} from "react";
+import {
+    SEARCH_USERS,
+    SET_LOADING,
+    CLEAR_USERS,
+    GET_USER,
+    GET_REPOS
+} from "../types";
 
-const githubContext = createContext();
+export default (state, action) => {
+    switch (action.type) {
+        case SEARCH_USERS:
+            return {
+                ...state,
+                users:action.payload,
+                loading: false,
+            }
+        case SET_LOADING:
+            return {
+                ...state,
+                loading:true,
+            }
 
-export default githubContext;
+        default:
+            return state;
+
+    }
+}
